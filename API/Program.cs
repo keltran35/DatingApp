d817ecc996ea4ae192f1cd0e4dca204f737f2e9f
Kel <kelbintran35@gmail.com>
2023-05-31 13:42:38 +0700

    Error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +16,13 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-  app.UseSwagger();
-  app.UseSwaggerUI();
-}
+app.UseMiddleware<ExceptionMiddleware>();
+
+// if (app.Environment.IsDevelopment())
+// {
+//   app.UseSwagger();
+//   app.UseSwaggerUI();
+// }
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
