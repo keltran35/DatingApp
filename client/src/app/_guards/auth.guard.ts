af3fc2dfc4d01842f3351b058b1f811715539018
Kel <kelbintran35@gmail.com>
2023-06-07 14:48:05 +0700

    Updating resources

--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -1,5 +1,5 @@
 import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 import { map } from 'rxjs';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
@@ -8,7 +8,6 @@ import { ToastrService } from 'ngx-toastr';
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AccountService);
   const toastr = inject(ToastrService);
-  const router = inject(Router);
 
   return authService.currentUser$.pipe(
     map((user) => {
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -18,8 +18,6 @@ export class AppComponent implements OnInit {
     this.setCurrentUser();
   }
 
-
-
   setCurrentUser() {
     const userString = localStorage.getItem('user');
     if (!userString) return;
